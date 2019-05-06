5d8e788733f22affb062ffb4076cc3ba196e3b64
Gusti Ngurah <gusti.ngurah.mertayasa@gmail.com>
2019-05-06 02:54:59 -0700

    first commit

--- computervision.php
+++ computervision.php
@@ -2,6 +2,7 @@
 if (isset($_POST['submit'])) {
   if (isset($_POST['url'])) {
     $url = $_POST['url'];
+     // $url = "https://timedotcom.files.wordpress.com/2018/09/bill-gates-africa.jpg";
   } else {
     header("Location: index.php");
   }
@@ -51,7 +52,7 @@ if (isset($_POST['submit'])) {
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php">Home</a>
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" onclick="processImage()">Home</a>
             </li>
             <li class="nav-item mx-0 mx-lg-1">
               <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="https://www.linkedin.com/in/gusti-ngurah-mertayasa-342bab166/">About Me</a>
@@ -74,61 +75,71 @@ if (isset($_POST['submit'])) {
         <span class="border-top my-3"></span>
       </div>
   
-   <script type="text/javascript">
-            $(document).ready(function () {
-            // **********************************************
-            // *** Update or verify the following values. ***
-            // **********************************************
-            // Replace <Subscription Key> with your valid subscription key.
-            var subscriptionKey = "5244d0b5afb846009d2d0c8f14313367";
-            // You must use the same Azure region in your REST API method as you used to
-            // get your subscription keys. For example, if you got your subscription keys
-            // from the West US region, replace "westcentralus" in the URL
-            // below with "westus".
-            //
-            // Free trial subscription keys are generated in the "westus" region.
-            // If you use a free trial subscription key, you shouldn't need to change
-            // this region.
-            var uriBase =
+<script type="text/javascript">
+    function processImage() {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+ 
+        // Replace <Subscription Key> with your valid subscription key.
+        var subscriptionKey = "109204d199cc4f4ab2642040d7e4813d";
+ 
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        var uriBase =
             "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-            // Request parameters.
-            var params = {
-                "visualFeatures": "Categories,Description,Color",
-                "details": "",
-                "language": "en",
-            };
-            // Display the image.
-            var sourceImageUrl = "<?php echo $url ?>";
-            document.querySelector("#sourceImage").src = sourceImageUrl;
-            // Make the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-                type: "POST",
-                // Request body.
-                data: '{"url": ' + '"' + sourceImageUrl + '"}',
-            })
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                $("#responseTextArea").val(JSON.stringify(data, null, 2));
-                // console.log(data);
-                // var json = $.parseJSON(data);
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        var sourceImageUrl = "<?php echo $url ?>";
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
                 $("#description").text(data.description.captions[0].text);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " :
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
                 errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" :
+            errorString += (jqXHR.responseText === "") ? "" :
                 jQuery.parseJSON(jqXHR.responseText).message;
-                alert(errorString);
-            });
+            alert(errorString);
         });
-    </script>
+    };
+</script>
+
 <br>
 <div id="wrapper" style="width:1020px; display:table;">
   <div id="jsonOutput" style="width:600px; display:table-cell;">
--- create-table.sql
+++
@@ -1,7 +0,0 @@
-create table [dbo].[Registration](
-    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
-    nama VARCHAR(50),
-    nim VARCHAR(50),
-    npk VARCHAR(50),
-    date DATE
-);
